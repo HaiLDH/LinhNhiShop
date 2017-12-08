eb9b6f0733a733fa0f73941307b9958080620ee5
AKay <haild.dev8@gmail.com>
2017-12-08 23:08:41 +0700

    create login form.

---
+++ LinhNhiShop/LinhNhiShop.Web/app/components/login/loginAdminController.js
@@ -0,0 +1,11 @@
+﻿(function (app) {
+    app.controller('loginAdminController', loginAdminController);
+
+    loginAdminController.$inject = ['$scope','$state']
+
+    function loginAdminController($scope, $state) {
+        $scope.loginSubmit = function () {
+            $state.go('home');
+        }
+    }
+})(angular.module('linhnhishop'));
