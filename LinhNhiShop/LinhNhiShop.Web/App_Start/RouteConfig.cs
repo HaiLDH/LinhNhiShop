26dbd148c015e432ada6cc38fc5588320e5fdd41
AKay <haild.dev8@gmail.com>
2017-12-14 02:40:37 +0700

    fix cshtml file client template.

--- LinhNhiShop/LinhNhiShop.Web/App_Start/RouteConfig.cs
+++ LinhNhiShop/LinhNhiShop.Web/App_Start/RouteConfig.cs
@@ -10,40 +10,38 @@ namespace LinhNhiShop.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-               name: "Login",
-               url: "dang-nhap.html",
-               defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional },
-               namespaces: new string[] { "LinhNhiShop.Web.Controller" }
-           );
+                name: "Login",
+                url: "dang-nhap.html",
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional },
+                namespaces: new string[] { "LinhNhiShop.Web.Controllers" }
+            );
 
             routes.MapRoute(
-               name: "About",
-               url: "gioi-thieu.html",
-               defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional },
-               namespaces: new string[] { "LinhNhiShop.Web.Controller" }
-           );
-
-
+                name: "About",
+                url: "gioi-thieu.html",
+                defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional },
+            namespaces: new string[] { "LinhNhiShop.Web.Controllers" }
+            );
 
             routes.MapRoute(
-                name: "Product Category",
-                url: "{alias}.p-{id}.html",
+                name: "ProductCategory",
+                url: "{alias}.pc-{id}.html",
                 defaults: new { controller = "Product", action = "Category", id = UrlParameter.Optional },
-                namespaces: new string[] { "LinhNhiShop.Web.Controller" }
+                namespaces: new string[] { "LinhNhiShop.Web.Controllers" }
             );
 
             routes.MapRoute(
                 name: "Product",
-                url: "{alias}.pc-{id}.html",
-                defaults: new { controller = "Product", action = "Detail", id = UrlParameter.Optional },
-                namespaces: new string[] { "LinhNhiShop.Web.Controller" }
+                url: "{alias}.p-{id}.html",
+                defaults: new { controller = "Product", action = "Detail", productId = UrlParameter.Optional },
+                namespaces: new string[] { "LinhNhiShop.Web.Controllers" }
             );
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-                namespaces: new string[] { "LinhNhiShop.Web.Controller" }
+                namespaces: new string[] { "LinhNhiShop.Web.Controllers" }
             );
         }
     }
